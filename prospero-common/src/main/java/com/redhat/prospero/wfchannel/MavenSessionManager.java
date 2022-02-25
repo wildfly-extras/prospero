3c8db90216a82aa662e43096bd7db2eae68bd266
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2022-02-25 14:00:04 +0000

    Enable file:// maven repositories

--- prospero-common/src/main/java/com/redhat/prospero/wfchannel/MavenSessionManager.java
+++ prospero-common/src/main/java/com/redhat/prospero/wfchannel/MavenSessionManager.java
@@ -30,6 +30,7 @@ import org.eclipse.aether.impl.DefaultServiceLocator;
 import org.eclipse.aether.repository.LocalRepository;
 import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
 import org.eclipse.aether.spi.connector.transport.TransporterFactory;
+import org.eclipse.aether.transport.file.FileTransporterFactory;
 import org.eclipse.aether.transport.http.HttpTransporterFactory;
 import org.jboss.galleon.ProvisioningException;
 
@@ -55,6 +56,7 @@ public class MavenSessionManager {
         final DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator();
         locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class);
         locator.addService(TransporterFactory.class, HttpTransporterFactory.class);
+        locator.addService(TransporterFactory.class, FileTransporterFactory.class);
         locator.setErrorHandler(new DefaultServiceLocator.ErrorHandler() {
             @Override
             public void serviceCreationFailed(Class<?> type, Class<?> impl, Throwable exception) {
