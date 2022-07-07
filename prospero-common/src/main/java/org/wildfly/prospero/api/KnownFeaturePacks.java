bdbc7b17cc0de741078f2e2fa1cceb3fde2e490e
Bartosz Spyrko-Smietanko <spyrkob@gmail.com>
2022-07-07 16:07:42 +0100

    [#93] Empty list of known fpls is used if source file is not found

--- prospero-common/src/main/java/org/wildfly/prospero/api/KnownFeaturePacks.java
+++ prospero-common/src/main/java/org/wildfly/prospero/api/KnownFeaturePacks.java
@@ -7,12 +7,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.jboss.logging.Logger;
 import org.wildfly.prospero.model.KnownFeaturePack;
 
 /**
  * Defines well known Galleon feature packs.
  */
 public abstract class KnownFeaturePacks {
+
+    private static final Logger logger = Logger.getLogger(KnownFeaturePacks.class);
 
     private static final Map<String, KnownFeaturePack> nameMap = new HashMap<>();
 
@@ -33,11 +36,15 @@ public abstract class KnownFeaturePacks {
         // Update also the list of names in UsageMessages.properties when modifying this.
         //
         try {
-            final URL resource = KnownFeaturePacks.class.getClassLoader().getResource("known-repositories.yaml");
-
-            final List<KnownFeaturePack> knownFeaturePacks = KnownFeaturePack.readConfig(resource);
-            for (KnownFeaturePack fp : knownFeaturePacks) {
-                nameMap.put(fp.getName(), fp);
+            final URL knownRepoUrl = KnownFeaturePacks.class.getClassLoader().getResource("known-repositories.yaml");
+            if (knownRepoUrl == null) {
+                logger.debug("No known repositories found");
+            } else {
+                logger.debug("Loading known repositories from: " + knownRepoUrl);
+                final List<KnownFeaturePack> knownFeaturePacks = KnownFeaturePack.readConfig(knownRepoUrl);
+                for (KnownFeaturePack fp : knownFeaturePacks) {
+                    nameMap.put(fp.getName(), fp);
+                }
             }
         } catch (IOException e) {
             throw new IllegalStateException(e);
