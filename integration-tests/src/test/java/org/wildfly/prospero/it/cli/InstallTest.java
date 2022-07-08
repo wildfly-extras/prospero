b9582d73aa874d5a4505a4d6be5f4cc24a222505
Tomas Hofman <thofman@redhat.com>
2022-07-08 10:34:10 +0200

    Run CLI integration tests in separate processes

--- integration-tests/src/test/java/org/wildfly/prospero/it/cli/InstallTest.java
+++ integration-tests/src/test/java/org/wildfly/prospero/it/cli/InstallTest.java
@@ -3,26 +3,23 @@ package org.wildfly.prospero.it.cli;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import org.wildfly.prospero.cli.CliMain;
 import org.wildfly.prospero.cli.ReturnCodes;
 import org.wildfly.prospero.cli.commands.CliConstants;
-import org.wildfly.prospero.it.AcceptingConsole;
+import org.wildfly.prospero.it.ExecutionUtils;
 import org.wildfly.prospero.test.MetadataTestUtils;
-import picocli.CommandLine;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class InstallTest {
 
     @Rule
     public TemporaryFolder tempDir = new TemporaryFolder();
 
-    File targetDir;
+    private File targetDir;
 
     @Before
     public void setUp() throws IOException {
@@ -30,17 +27,17 @@ public class InstallTest {
     }
 
     @Test
-    public void testInstallWithProvisionConfig() throws IOException {
+    public void testInstallWithProvisionConfig() throws Exception {
         URL provisionConfig = MetadataTestUtils.prepareProvisionConfigAsUrl("channels/wfcore-19-base.yaml");
 
-        CommandLine commandLine = CliMain.createCommandLine(new AcceptingConsole());
-        int returnCode = commandLine.execute(CliConstants.INSTALL,
-                CliConstants.PROVISION_CONFIG, provisionConfig.getPath(),
-                CliConstants.FPL, "wildfly-core@maven(org.jboss.universe:community-universe):19.0",
-                CliConstants.REMOTE_REPOSITORIES,
-                "https://repo1.maven.org/maven2/,https://repository.jboss.org/nexus/content/groups/public-jboss",
-                CliConstants.DIR, targetDir.getAbsolutePath());
-
-        assertThat(returnCode).isEqualTo(ReturnCodes.SUCCESS);
+        ExecutionUtils.prosperoExecution(CliConstants.INSTALL,
+                        CliConstants.PROVISION_CONFIG, provisionConfig.getPath(),
+                        CliConstants.FPL, "wildfly-core@maven(org.jboss.universe:community-universe):19.0",
+                        CliConstants.REMOTE_REPOSITORIES,
+                        "https://repo1.maven.org/maven2/,https://repository.jboss.org/nexus/content/groups/public-jboss",
+                        CliConstants.DIR, targetDir.getAbsolutePath())
+                .withTimeLimit(10, TimeUnit.MINUTES)
+                .execute()
+                .assertReturnCode(ReturnCodes.SUCCESS);
     }
 }
