d254f47cd13e374c611129640fa8124690ffd5e7
Parul Sharma <parsharm@redhat.com>
2023-08-10 23:44:49 +0530

    Updated ChannelAddCommand.java according to the previous changes

--- prospero-cli/src/main/java/org/wildfly/prospero/cli/commands/channel/ChannelAddCommand.java
+++ prospero-cli/src/main/java/org/wildfly/prospero/cli/commands/channel/ChannelAddCommand.java
@@ -20,17 +20,25 @@ package org.wildfly.prospero.cli.commands.channel;
 import org.wildfly.channel.Channel;
 import org.wildfly.channel.ChannelManifestCoordinate;
 import org.wildfly.channel.Repository;
+import org.wildfly.channel.maven.ChannelCoordinate;
 import org.wildfly.prospero.actions.MetadataAction;
 import org.wildfly.prospero.api.ArtifactUtils;
-import org.wildfly.prospero.cli.ActionFactory;
+import org.wildfly.prospero.api.MavenOptions;
+import org.wildfly.prospero.api.ProvisioningDefinition;
 import org.wildfly.prospero.cli.CliConsole;
-import org.wildfly.prospero.cli.CliMessages;
+import org.wildfly.prospero.cli.ActionFactory;
 import org.wildfly.prospero.cli.RepositoryDefinition;
+import org.wildfly.prospero.cli.CliMessages;
 import org.wildfly.prospero.cli.ReturnCodes;
+import org.wildfly.prospero.cli.ArgumentParsingException;
 import org.wildfly.prospero.cli.commands.AbstractCommand;
+import org.wildfly.prospero.cli.commands.AbstractInstallCommand;
 import org.wildfly.prospero.cli.commands.CliConstants;
 import picocli.CommandLine;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
@@ -38,17 +46,20 @@ import java.util.Optional;
 @CommandLine.Command(name = CliConstants.Commands.ADD, sortOptions = false)
 public class ChannelAddCommand extends AbstractCommand {
 
-    @CommandLine.Option(names = CliConstants.CHANNEL_NAME, required = true)
+    @CommandLine.Option(names = CliConstants.CHANNEL_NAME)
     private String channelName;
 
-    @CommandLine.Option(names = CliConstants.CHANNEL_MANIFEST, required = true)
+    @CommandLine.Option(names = CliConstants.CHANNEL_MANIFEST)
     private String gavUrlOrPath;
 
     @CommandLine.Option(names = CliConstants.DIR)
     private Optional<Path> directory;
 
-    @CommandLine.Option(names = CliConstants.REPOSITORIES, split = ",", paramLabel = CliConstants.REPO_URL, required = true)
+    @CommandLine.Option(names = CliConstants.REPOSITORIES, split = ",", paramLabel = CliConstants.REPO_URL)
     private List<String> repositoryDefs;
+
+    @CommandLine.Option(names = CliConstants.CHANNEL)
+    private Optional<String> channelUrlOrPath;
 
     public ChannelAddCommand(CliConsole console, ActionFactory actionFactory) {
         super(console, actionFactory);
@@ -58,16 +69,41 @@ public class ChannelAddCommand extends AbstractCommand {
     public Integer call() throws Exception {
         Path installationDirectory = determineInstallationDirectory(directory);
         List<Repository> repositories;
-        repositories = RepositoryDefinition.from(repositoryDefs);
+        List<Channel> channels = null;
+        if(channelUrlOrPath.isPresent()) {
+            final ChannelCoordinate coord = ArtifactUtils.channelCoordFromString(channelUrlOrPath.get());
+            checkFileExists(coord.getUrl(), channelUrlOrPath.get());
+            ProvisioningDefinition provisioningDefinition = ProvisioningDefinition.builder()
+                    .setChannelCoordinates(channelUrlOrPath.get())
+                    .build();
+            channels = AbstractInstallCommand.resolveChannels(provisioningDefinition,
+                    MavenOptions.DEFAULT_OPTIONS);
+        } else if (!(gavUrlOrPath.isEmpty() || repositoryDefs.isEmpty())) {
+            repositories = RepositoryDefinition.from(repositoryDefs);
 
-        console.println(CliMessages.MESSAGES.subscribeChannel(installationDirectory, channelName));
+            console.println(CliMessages.MESSAGES.subscribeChannel(installationDirectory, channelName));
+            ChannelManifestCoordinate manifest = ArtifactUtils.manifestCoordFromString(gavUrlOrPath);
+                channels = List.of(new Channel(channelName, null, null, repositories, manifest, null, null));
+            } else {
+                return ReturnCodes.INVALID_ARGUMENTS;
+            }
+            try (final MetadataAction metadataAction = actionFactory.metadataActions(installationDirectory)) {
+                for (Channel channel : channels) {
+                    metadataAction.addChannel(channel);
+                }
+            }
 
-        ChannelManifestCoordinate manifest = ArtifactUtils.manifestCoordFromString(gavUrlOrPath);
-        Channel channel = new Channel(channelName, null, null, repositories, manifest, null, null);
-        try (MetadataAction metadataAction = actionFactory.metadataActions(installationDirectory)) {
-            metadataAction.addChannel(channel);
+            console.println(CliMessages.MESSAGES.channelAdded(channelName));
+            return ReturnCodes.SUCCESS;
         }
-        console.println(CliMessages.MESSAGES.channelAdded(channelName));
-        return ReturnCodes.SUCCESS;
+
+        private void checkFileExists(URL resourceUrl, String argValue) throws ArgumentParsingException {
+            if (resourceUrl != null) {
+                try (InputStream is = resourceUrl.openStream()) {
+                    // OK ignore, just need to check it exists
+                } catch (IOException e) {
+                    throw CliMessages.MESSAGES.missingRequiresResource(argValue);
+                }
+            }
+        }
     }
-}
